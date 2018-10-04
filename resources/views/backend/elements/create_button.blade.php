abd9d2309cb7ae313766d64cf4934a2b31d4c9b6
Vermouth5214 <oeidonny.winarto@gmail.com>
2018-10-04 14:24:20 +0700

    ubah a jadi vu

--- resources/views/backend/elements/create_button.blade.php
+++ resources/views/backend/elements/create_button.blade.php
@@ -3,7 +3,7 @@
 	$userinfo = Session::get('userinfo');
 	$access_control = Session::get('access_control');
 	if (!empty($access_control)) :
-		if ($access_control[$userinfo['user_level_id']][$segment] == "a"):
+		if ($access_control[$userinfo['user_level_id']][$segment] != "v"):
 ?>
 	<a href="<?=url($url);?>" class="btn-index btn btn-primary btn-block" title="Add"><i class="fa fa-plus"></i>&nbsp; Add</a>
 <?php
